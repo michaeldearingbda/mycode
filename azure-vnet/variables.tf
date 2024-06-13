1c758126f626a25ebe03aa124d984bc7e9041138
michaeldearingbda <student@bchd.1b7af11b-27ff-4cdf-9700-6a96fe05e7b9>
2024-06-13 19:05:51 +0000

    terraform and Azure virtual networks

---
+++ azure-vnet/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  default     = "mikeinfra"
+  type        = string
+  description = "Name for this infrastructure"
+}
+
+variable "location" {
+  type        = string
+  description = "Name of the region for this infrastructure"
+  default     = "westus2"
+}
+
+variable "address_space" {
+  type        = string
+  description = "Cidr range for the Virtual Network"
+  default     = "10.10.0.0/16"
+}
